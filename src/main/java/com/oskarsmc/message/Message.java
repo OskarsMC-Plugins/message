286535c5cc1abd83388af613cc43c3bdc279615d
OskarZyg <56176746+OskarZyg@users.noreply.github.com>
2021-05-04 22:49:27 +0100

    BStats Integration

--- src/main/java/com/oskarsmc/message/Message.java
+++ src/main/java/com/oskarsmc/message/Message.java
@@ -5,15 +5,20 @@ import com.oskarsmc.message.command.MessageBrigadier;
 import com.oskarsmc.message.command.ReplyBrigadier;
 import com.oskarsmc.message.command.SocialSpyBrigadier;
 import com.oskarsmc.message.configuration.MessageSettings;
-import com.oskarsmc.message.util.VersionUtils;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.oskarsmc.message.event.MessageEvent;
+import com.oskarsmc.message.util.StatsUtils;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
+import org.bstats.charts.SingleLineChart;
+import org.bstats.velocity.Metrics;
 import org.slf4j.Logger;
 
 import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Plugin(
         id = "message",
@@ -35,6 +40,11 @@ public class Message {
     private @DataDirectory
     Path dataDirectory;
 
+    @Inject
+    private Metrics.Factory metricsFactory;
+
+    private AtomicInteger messagesSent = new AtomicInteger(0);
+
     private MessageSettings messageSettings;
     private MessageBrigadier messageBrigadier;
     private SocialSpyBrigadier socialSpyBrigadier;
@@ -43,6 +53,16 @@ public class Message {
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
         this.messageSettings = new MessageSettings(dataDirectory.toFile(), logger);
+
+        Metrics metrics = metricsFactory.make(this, StatsUtils.PLUGIN_ID);
+        metrics.addCustomChart(new SingleLineChart("messages_sent", new Callable<Integer>() {
+            @Override
+            public Integer call() {
+                int ret = messagesSent.get();
+                messagesSent.set(0);
+                return ret;
+            }
+        }));
 
         if (messageSettings.isEnabled()) {
             this.messageBrigadier = new MessageBrigadier(this.proxyServer, this.messageSettings);
@@ -53,5 +73,10 @@ public class Message {
             this.proxyServer.getEventManager().register(this, this.socialSpyBrigadier);
             this.proxyServer.getEventManager().register(this, this.replyBrigadier);
         }
+    }
+
+    @Subscribe
+    public void messageEvent(MessageEvent event) {
+        messagesSent.incrementAndGet();
     }
 }
