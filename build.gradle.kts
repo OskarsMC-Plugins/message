bda52c5ed2d90389ebb83afae6f09935f7ae4b1c
4drian3d <adrianed2004@gmail.com>
2021-11-30 11:12:13 -0500

    Update MiniMessage to 4.2.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("org.bstats:bstats-velocity:2.2.1")
     implementation("com.velocitypowered:velocity-api:3.0.1")
-    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+    implementation("net.kyori:adventure-text-minimessage:4.2.0-SNAPSHOT")
     implementation("cloud.commandframework:cloud-velocity:1.5.0")
     implementation("cloud.commandframework:cloud-minecraft-extras:1.5.0")
     compileOnly("net.luckperms:api:5.3")
@@ -40,7 +40,7 @@ tasks {
             include {
                 it.moduleGroup == "org.bstats" || it.moduleGroup == "cloud.commandframework" || it.moduleGroup == "io.leangen.geantyref"
             }
-            include(dependency("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT"))
+            include(dependency("net.kyori:adventure-text-minimessage:4.2.0-SNAPSHOT"))
         }
         relocate("org.bstats", "com.oskarsmc.message.relocated.bstats")
         relocate("net.kyori.adventure.text.minimessage", "com.oskarsmc.message.relocated.minimessage")
